da24471fd2d6098cb31cef256513c70ebe8fc0c7
rakshitha k <raks>
2024-09-19 20:10:52 +0530

    test

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-struct num
-{
-    int num1,num2;
-}n1;
 int input()
 {
-    printf("Enter 2 numbers\n");
-    scanf("%d",&n1.num1);
-    scanf("%d",&n1.num2);
+    int a;
+    scanf("%d",&a);
+    return a;
 }
 int add(int a, int b)
 {
@@ -16,13 +12,12 @@ int add(int a, int b)
 }
 void output(int a, int b, int sum)
 {
-     printf("sum of %d and %d is %d",a,b,sum);
+     printf("The sum of %d and %d is %d",a,b,sum);
 }
 int main()
 {
-    int a=n1.num1;
-    int b=n1.num2;
-    input();
+    int a=input();
+    int b=input();
     int sum=add(a,b);
     output(a,b,sum);
     return 0;
