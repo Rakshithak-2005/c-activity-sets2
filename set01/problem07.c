165346822716179b9dfe911ca1495a60cebc897c
rakshitha k <raks>
2024-09-20 20:20:23 +0530

    test

---
+++ set01/problem07.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int input_n()
+{
+    int n;
+    scanf("%d",&n);
+    return n;
+}
+int sum_n_nos(int n)
+{ 
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    return sum;
+}
+void output(int n, int sum)
+{
+    printf("Sum of %d natural numbers is %d",n,sum);
+}
+int main()
+{
+    int n=input_n();
+    int sum=sum_n_nos(n);
+    output(n,sum);
+    return 0;
+}
