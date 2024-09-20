d067e1018e27ce12f42300a63d1a67ad62b732ee
rakshitha k <raks>
2024-09-20 19:58:27 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,36 @@
-
+#include <stdio.h>
+int input()
+{
+   int a;
+   scanf("%d",&a);
+   return a;
+}
+int compare(int a, int b, int c)
+{
+   if(a>b && a>c)
+   {
+    return a;
+   }
+   else if(b>c)
+   {
+    return b;
+   }
+   else
+   {
+    return c;
+   }
+}
+void output(int a, int b, int c, int largest)
+{
+   printf("The largest of %d,%d and %d is %d.",a,b,c,largest);
+}
+int main()
+{
+ int a,b,c,largest;
+ a=input();
+ b=input();
+ c=input();
+ largest=compare(a,b,c);
+ output(a,b,c,largest);
+ return 0;
+}
