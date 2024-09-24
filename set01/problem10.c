611ba992f6a8812a9c1dce7ebf6b9ae147412278
rakshitha k <raks>
2024-09-24 19:51:54 +0530

    test

---
+++ set01/problem10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void input_two_strings(char *string1, char *string2)
+{
+
+}
+int stringcompare(char *string1, char *string2)
+{
+
+}
+void output(char *string1, char *string2, int result)
+{
+
+}
+int main()
+{
+    return 0;
+}
